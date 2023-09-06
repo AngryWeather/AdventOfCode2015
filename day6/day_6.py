81bb50d3269df7bff93d3c32a09082aec53e35c6
Weronika <WerRic96@protonmail.com>
2023-09-06 03:12:30 +0200

    Finish day6 part1

---
+++ day6/day_6.py
@@ -0,0 +1,47 @@
+grid_size = 1000
+grid = [[0 for i in range(grid_size)] for j in range(grid_size)]
+
+
+def light_lights(instruction):
+    # get instructions
+    command = instruction[0]
+    start = instruction[1].split(",")
+    start_x = int(start[0])
+    start_y = int(start[1])
+    end = instruction[3].split(",")
+    end_x = int(end[0])
+    end_y = int(end[1])
+
+    i = start_y
+    j = start_x
+
+    while i <= end_y:
+        while j <= end_x:
+            match command:
+                case "on":
+                    grid[i][j] = 1
+                case "off":
+                    grid[i][j] = 0
+                case "toggle":
+                    grid[i][j] ^= 1
+            j += 1
+
+        i += 1
+        j = start_x
+
+
+def get_count(instructions):
+    for instruction in instructions:
+        light_lights(instruction)
+    return (sum(n.count(1) for n in grid))
+
+
+if __name__ == "__main__":
+    input = []
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            instruction = line.strip().split(" ")
+            if instruction[0] == "turn":
+                instruction.remove(instruction[0])
+            input.append(instruction)
+    print(get_count(input))
