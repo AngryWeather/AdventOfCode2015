ab5962b8906a18394f37a92c9f47239ff6986c9b
Weronika <WerRic96@protonmail.com>
2023-10-18 23:20:45 +0200

    Finish part 1

---
+++ day10/day_10.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+if __name__ == "__main__":
+    input = list(str(3113322113))
+    iterations = 40
+    result = ""
+    current = 0
+    j = 0
+    res = result
+
+    while j < iterations:
+        for i, k in enumerate(input):
+            if i + 1 < len(input) and input[current] != input[i + 1]:
+                counter = Counter(input[current:i+1])
+                current = i + 1
+
+            if i + 1 == len(input) and input[-1] == input[current]:
+                counter = Counter(input[current:i+1])
+
+            elif i + 1 == len(input) and input[-1] != input[current]:
+                counter = Counter[input[-1]]
+
+            for item in counter.items():
+                result += str(item[1]) + item[0]
+
+            counter.clear()
+
+        j += 1
+        current = 0
+        input = list(result)
+        res = result
+        result = ""
+
+    print("length: ", len(res))
