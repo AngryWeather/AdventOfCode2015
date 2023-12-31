1d9782d50458a01111be53dde0e7700bd1082911
Weronika <WerRic96@protonmail.com>
2023-12-31 14:05:21 +0100

    Finish part 1

---
+++ day13/day_13.py
@@ -0,0 +1,41 @@
+import itertools
+
+
+def calculate_total(perm):
+    total = 0
+    for n in range(len(perm)):
+        # print(len(perm))
+        # print(n, (n+1) % len(perm))
+        total += relationship[(perm[n], perm[(n+1) % len(perm)])]
+        total += relationship[(perm[(n+1) % len(perm)], perm[n])]
+    return total
+
+
+if __name__ == "__main__":
+    input = []
+    with open("input.txt") as f:
+        for line in f.readlines():
+            input.append(line)
+
+    relationship = {}
+    people = []
+
+    for i in input:
+        sentence = i.split()
+        person = sentence[0]
+        if person not in people:
+            people.append(person)
+        target = sentence[10].strip(".")
+        if sentence[2] == "gain":
+            points = int(sentence[3])
+        else:
+            points = int(sentence[3])*-1
+        relationship[(person, target)] = points
+
+        perm = list(itertools.permutations(people))
+
+    max_total = 0
+    for n in perm:
+        total = calculate_total(n)
+        max_total = max(max_total, total)
+    print(max_total)
