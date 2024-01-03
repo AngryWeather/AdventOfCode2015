096b2fdd9fb2bfe17cfc01f5c16333e19d256df9
Weronika <WerRic96@protonmail.com>
2024-01-03 20:30:38 +0100

    Finish day 13

--- day13/day_13.py
+++ day13/day_13.py
@@ -6,8 +6,8 @@ def calculate_total(perm):
     for n in range(len(perm)):
         # print(len(perm))
         # print(n, (n+1) % len(perm))
-        total += relationship[(perm[n], perm[(n+1) % len(perm)])]
-        total += relationship[(perm[(n+1) % len(perm)], perm[n])]
+        total += relationship[(perm[n], perm[(n + 1) % len(perm)])]
+        total += relationship[(perm[(n + 1) % len(perm)], perm[n])]
     return total
 
 
@@ -20,16 +20,20 @@ if __name__ == "__main__":
     relationship = {}
     people = []
 
+    people.append("me")
+
     for i in input:
         sentence = i.split()
         person = sentence[0]
         if person not in people:
             people.append(person)
+            relationship[("me", person)] = 0
+            relationship[(person, "me")] = 0
         target = sentence[10].strip(".")
         if sentence[2] == "gain":
             points = int(sentence[3])
         else:
-            points = int(sentence[3])*-1
+            points = int(sentence[3]) * -1
         relationship[(person, target)] = points
 
         perm = list(itertools.permutations(people))
